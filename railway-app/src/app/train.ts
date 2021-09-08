5970f2f96c89015fd5cf67edbbc473174bd8a804
Tejas Nambiar <76155224+TejasNambiar@users.noreply.github.com>
2021-09-08 19:23:55 +0530

    Revert "creted railway-app"
    This reverts commit 3fac20882b11cdfd2f960fd4988913dd0a791dfc.

--- railway-app/src/app/train.ts
+++
@@ -1,9 +0,0 @@
-export class Trains {
-    trainName !: String;
-    trainNumber !: Number;
-    startStation !: String;
-    destination !:String;
-    arrivalTime !:String;
-    departureTime !:String;
-    date !:Date
-}
