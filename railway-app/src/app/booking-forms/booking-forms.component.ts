26c780d56054ec37abf97a0dc96430a5ba05ae17
Tejas Nambiar <76155224+TejasNambiar@users.noreply.github.com>
2021-09-09 23:16:29 +0530

    Added Stepper-Form for Booking
    Completed basic form template for Booking.  Able to push data into MongoDB

--- railway-app/src/app/booking-forms/booking-forms.component.ts
+++ railway-app/src/app/booking-forms/booking-forms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-booking-forms',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingFormsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bookService:BookingService, private formBuild:FormBuilder) { }
+
+  bookingArray = {
+    fullName:'',
+    tier:'',
+    email:'',
+    start:'',
+    destination:'',
+    trainName:'',
+    date:'',
+    adhaar:'',
+    cardHolder:'',
+    cardNumber:'',
+    cvv:''
+  }
 
   ngOnInit(): void {
   }
 
+  addBooking(){
+    this.bookService.addBooking(this.bookingArray)
+      .subscribe(booking => this.bookingArray = booking)
+  }
 }
--- railway-app/src/app/booking.service.ts
+++ railway-app/src/app/booking.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  // this ur url to connect to mongodb
+  urlBooking = 'http://localhost:3050/api/booking'
+  //urlTrain = 'assets/data/trains.json'
+  getBooking():Observable<any>{
+
+    console.log("calling getBooking: "+ this.http.get<any>(this.urlBooking))
+    // this will set ur header to GET 
+    // return this.http.get(this.urlTrain).pipe(map(resp=>resp))
+    return this.http.get<any>(this.urlBooking)
+  }
+  
+  // add contact method
+  // this will set ur header to POST
+  addBooking(newBooking:any):Observable<any>{
+    var headers = new HttpHeaders()
+    console.log("inside addTrain -> newTrain:",newBooking.toString())
+    headers.append('Contact-Type','application/json')
+    return this.http.post<any>(this.urlBooking, newBooking,{headers:headers})
+    .pipe(map(resp=>resp))
+  }
+
+  getBookingId(id:any):Observable<any>{
+    console.log("Booking id: "+id)
+    return this.http.get<any>(this.urlBooking+'/'+id)
+    .pipe(map((resp=>resp)))
+  }
+
+  deleteBooking(id:any):Observable<any>{
+    console.log("deleteTrain id: "+id)
+    return this.http.delete<any>(this.urlBooking+'/'+id)
+    .pipe(map((resp=>resp)))
+  }
 }
